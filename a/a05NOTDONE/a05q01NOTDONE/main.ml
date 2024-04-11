b7edbcd09b75ab966f8048f5c1b5b8412507178e
CarlDalebout <cadalebout1@couger.ciss.edu>
2024-04-11 14:51:17 -0500

    updates

--- a/a05NOTDONE/a05q01NOTDONE/main.ml
+++ a/a05NOTDONE/a05q01NOTDONE/main.ml
@@ -1,8 +1,21 @@
-let rec subseteq = fun list1 -> fun list2 ->
+let rec seteq = fun list1 -> fun list2 ->
   match list1 with
   | [] -> if list2 = [] then 
       true
     else
       false
   | x::xs -> 
+    let rec checklist2 = fun target -> fun list2 ->
+      match list2 with
+      | [] -> false
+      | y::ys -> if y = target then 
+            true
+          else
+            checklist2 target ys 
+    in
+    
+    if checklist2 x list2 then 
+      seteq xs list2
+    else
+      false
 ;;
